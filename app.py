59280693dfc452e527054b6558924c44c77c8ad8
MCSchedula <claudetalbot4@gmail.com>
2026-04-07 08:21:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -610,6 +610,7 @@ def init_db():
             Setting.set('maxTeamLevelDiff', 0.4)
             Setting.set('maxMatchLevelDiff', 0.49)
 
+init_db()
+
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
